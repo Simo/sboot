1d1161b4801da798b26416398bc58d4462193556
Simone Bierti <sbierti@gmail.com>
2017-05-17 11:42:07 +0200

    corretti test

---
+++ .sboot-repo/src/WriterTest.java
@@ -0,0 +1,53 @@
+package tex.software.prova.business.dtos;
+
+import tex.software.prova.persistence.entities.Actor;
+
+public class ActorDTO {
+
+    
+    private Integer actorId;
+    
+    private String name;
+    
+    private Integer age;
+    
+
+    public ActorDTO(){}
+
+	public ActorDTO(Actor entity){
+						this.actorId = entity.getActorId();
+								this.name = entity.getName();
+								this.age = entity.getAge();
+					}
+
+    public Actor convert() {
+      Actor entity = new Actor();
+						entity.setActorId(this.actorId);
+			entity.setName(this.name);
+			entity.setAge(this.age);
+    	return entity;
+    }
+
+  public Integer getActorId() {
+		return actorId;
+  }
+
+  public void setActorId(Integer actorId) {
+    this.actorId = actorId;
+  }
+  public String getName() {
+		return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  public Integer getAge() {
+		return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+}
--- spec/source_writer_spec.rb
+++ spec/source_writer_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Sboot::SourceWriter do
 
-    subject(:writer) { writer = Sboot::SourceWriter.new sboot_repo_path: 'sboot-repo'}
+    subject(:writer) { writer = Sboot::SourceWriter.new sboot_repo_path: '.sboot-repo'}
 
   describe 'initialize' do
 
@@ -19,16 +19,16 @@ describe Sboot::SourceWriter do
         # Il file deve essere stato salvato senza modifiche
         expect(File.read 'src/WriterTest.java').to eql(file_contents)
         # Il file deve essere stato salvato anche nel repository
-        expect(File.read 'sboot-repo/src/WriterTest.java').to eql(file_contents)
+        expect(File.read '.sboot-repo/src/WriterTest.java').to eql(file_contents)
     end
     
     it 'patch existing file' do
         # Creo il file nel repository
-        FileUtils.mkdir_p 'sboot-repo/src'
-        File.write 'sboot-repo/src/WriterTest.java', File.read("#{File.dirname __FILE__}/assets/source_v1.java")
+        FileUtils.mkdir_p '.sboot-repo/src'
+        File.write '.sboot-repo/src/WriterTest.java', File.read("#{File.dirname __FILE__}/assets/source_v1.java")
 
         # Creo il file sorgente pre-modifica (sorgente modificato)       
-        FileUtils.mkdir_p 'sboot-repo/src'
+        FileUtils.mkdir_p 'src'
         File.write 'src/WriterTest.java', File.read("#{File.dirname __FILE__}/assets/source_edited.java")
         
         # Salvo il nuovo file
@@ -36,13 +36,13 @@ describe Sboot::SourceWriter do
         writer.write_file 'src/WriterTest.java', file_v2
         
         # Il file nel repository deve essere stato aggiornato
-        expect(File.read 'sboot-repo/src/WriterTest.java').to eq(file_v2)
+        expect(File.read '.sboot-repo/src/WriterTest.java').to eq(file_v2)
 
         # Verifico che le modifiche non siano andate perse
         expect(File.read 'src/WriterTest.java').to eq(File.read("#{File.dirname __FILE__}/assets/source_patched.java"))
     end
     
-    after(:each) { Dir.glob(['src','sboot-repo']).each { |f| FileUtils.rm_rf f } }
+    after(:each) { Dir.glob(['src','.sboot-repo']).each { |f| FileUtils.rm_rf f } }
   end
 
 end
